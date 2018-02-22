9b7abc1ef66c5bbca09b9aa07f6e46cd1af520a0
jordanpeter <jordanpeter26@gmail.com>
2018-02-22 11:39:23 +0100

    save

---
+++ request.sql
@@ -0,0 +1,6 @@
+SELECT * FROM `students`;
+SELECT prenom FROM students;
+SELECT prenom, datenaissance, school FROM students;
+SELECT * FROM students WHERE genre = "F";
+SELECT prenom FROM students ORDER BY prenom DESC ;
+SELECT prenom FROM students ORDER BY prenom DESC LIMIT 2;
